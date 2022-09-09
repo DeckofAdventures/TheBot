0fd56037e896b039e479ab679a599f91d9935c17
Chris Broz <CBroz@datajoint.com>
2022-09-08 21:03:56 -0500

    Add /feedback command to post issues. Return URL. Duplicative loadenv

--- the_bot/bot.py
+++ the_bot/bot.py
@@ -18,31 +18,40 @@ logging.basicConfig(
 )
 
 # - Discord client -
+load_dotenv()
+load_dotenv("TheBot/the_bot/.env")
 # bot = hikari.GatewayBot(os.getenv("DISCORD_TOKEN") or "") # base hikari
 bot = lightbulb.BotApp(
-    os.getenv("DISCORD_TOKEN") or "",
-    default_enabled_guilds=(955796331375517787),  # only this server
+    token=os.getenv("DISCORD_TOKEN"),
+    # default_enabled_guilds=(os.getenv("GUILD_ID")),  # only this server
+    logs="DEBUG",
 )
 
 # - Bot events -
 @bot.command
 @lightbulb.option("desc", "Description")
 @lightbulb.option("title", "Title")
-@lightbulb.command("temp2", "Post GitHub Issue")
+@lightbulb.command("feedback", "Post GitHub Issue")
 @lightbulb.implements(lightbulb.SlashCommand)
-async def temp2(ctx):
+async def feedback(ctx):
+    if load_dotenv() or load_dotenv("TheBot/the_bot/.env"):
+        GITHUB_USER = os.environ.get("GITHUB_USER")
+        GITHUB_PASS = os.environ.get("GITHUB_TOKEN")
+        REPO_OWNER = os.environ.get("REPO_OWNER")
+        REPO_NAME = os.environ.get("REPO_NAME")
+        if not all([GITHUB_USER, GITHUB_PASS, REPO_OWNER, REPO_NAME]):
+            logger.error("Could not find all credentials")
+            return
+    else:
+        logger.error("Could not find env file")
+        return
+
     logger.info(f"New issue from {ctx.author.username}")  # or .user?
-
-    GITHUB_USER = os.environ.get("GITHUB_USER")
-    GITHUB_PASS = os.environ.get("GITHUB_PASS")
-    REPO_OWNER = os.environ.get("REPO_OWNER")
-    REPO_NAME = os.environ.get("REPO_NAME")
-
     issue = {
         "title": ctx.options.title,
-        "body": ctx.options.desc,
-        "assignee": GITHUB_USER,
-        "labels": ["Discord", "Under Consideration"],
+        "body": ctx.options.desc + f"\n\nPosted by {ctx.author.username}",
+        "assignee": GITHUB_USER,  # assigned to user who provided credentials
+        "labels": ["DiscordBot", "Under Consideration"],
     }
     url = "https://api.github.com/repos/%s/%s/issues" % (REPO_OWNER, REPO_NAME)
     # Create an authenticated session to create the issue
@@ -52,16 +61,24 @@ async def temp2(ctx):
     response = session.post(url, json.dumps(issue))
     if response.status_code == 201:
         logger.info('Successfully created Issue "%s"' % ctx.options.title)
-        resp_obj = json.loads(response.content)
-        logger.info("Response: ", resp_obj["url"])
+        resp_url = json.loads(response.content)["html_url"]
+        await ctx.respond(f"Issue URL: {resp_url}")
     else:
-        logger.info('Could not create Issue "%s"' % ctx.options.title)
-        logger.info("Response: ", response.content)
-
-    await ctx.respond("Conditional response here with issue #")
+        logger.error("Response: ", response.content)
+        await ctx.respond(
+            'Could not create Issue "%s". Please contact Admin.' % ctx.options.title
+        )
 
 
 def main():
     """Entrypoint for the Discord Bot."""
-    load_dotenv()
+    if load_dotenv() or load_dotenv("TheBot/the_bot/.env"):
+        logger.info("Credentials loaded")
+    else:
+        logger.error("Could not find credentials")
+        return
     bot.run()
+
+
+if __name__ == "__main__":
+    main()
